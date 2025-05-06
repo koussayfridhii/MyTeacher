8583457a35e74d46d1b7c33d0307dd5c30802515
koussayfridhii <koussayfridhi.kf@gmail.com>
2025-05-06 14:13:58 +0100

    refactor: Enhance server startup logging format for consistency

--- back/server.js
+++ back/server.js
@@ -34,4 +34,6 @@ app.get("/", (req, res) => {
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});
