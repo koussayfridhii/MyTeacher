70f588bb967c02f07c11d82ea197467d2c4af384
koussayfridhii <koussayfridhi.kf@gmail.com>
2025-05-06 23:09:27 +0100

    fix: Update CORS configuration to restrict allowed origins

--- back/server.js
+++ back/server.js
@@ -14,7 +14,25 @@ import streamTokenRouter from "./routes/streamTokenRoutes.js";
 connectDB();
 
 const app = express();
-app.use(cors({ origin: "*" }));
+const allowedOrigins = [
+  "https://my-teacher-6ner.vercel.app",
+  "http://localhost:3000/",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 // app.use(express.json());
 app.use(passport.initialize());
 import("./config/passport.js").then((mod) => mod.default(passport));
