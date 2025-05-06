cd5a4995044b6a6718d9645fcf47dd0935ebceba
koussayfridhii <koussayfridhi.kf@gmail.com>
2025-05-06 13:39:50 +0100

    feat: Refactor server.js for Vercel compatibility and update vercel.json configuration

--- back/server.js
+++ back/server.js
@@ -1,17 +1,21 @@
+// /api/index.js
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+
 import passport from "passport";
 import cors from "cors";
-import connectDB from "./config/db.js";
-import errorHandler from "./middleware/errorHandler.js";
-import authRoutes from "./routes/authRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import walletRoutes from "./routes/walletRoutes.js";
-import classRoutes from "./routes/classRoutes.js";
-import callRoutes from "./routes/callRoutes.js";
-import recordingRoutes from "./routes/recordingRoutes.js";
-import streamTokenRouter from "./routes/streamTokenRoutes.js";
+import serverless from "serverless-http";
+
+import connectDB from "../config/db.js";
+import errorHandler from "../middleware/errorHandler.js";
+import authRoutes from "../routes/authRoutes.js";
+import userRoutes from "../routes/userRoutes.js";
+import walletRoutes from "../routes/walletRoutes.js";
+import classRoutes from "../routes/classRoutes.js";
+import callRoutes from "../routes/callRoutes.js";
+import recordingRoutes from "../routes/recordingRoutes.js";
+import streamTokenRouter from "../routes/streamTokenRoutes.js";
 
 connectDB();
 
@@ -19,7 +23,7 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(passport.initialize());
-import("./config/passport.js").then((mod) => mod.default(passport));
+await import("../config/passport.js").then((mod) => mod.default(passport));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -28,10 +32,12 @@ app.use("/api/classes", classRoutes);
 app.use("/api/calls", callRoutes);
 app.use("/api/recordings", recordingRoutes);
 app.use("/api/stream", streamTokenRouter);
+
 app.get("/", (req, res) => {
-  res.send("working !!!!");
+  res.send("Serverless Express on Vercel is working!");
 });
+
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// **THIS** is what Vercel needs:
+export default serverless(app);
