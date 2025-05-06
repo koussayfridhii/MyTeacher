c938911e4cb026df197f84bd57a8e5f8bcfc4e0e
koussayfridhii <koussayfridhi.kf@gmail.com>
2025-05-06 14:17:07 +0100

    refactor: Comment out unused imports and middleware for future implementation

--- back/server.js
+++ back/server.js
@@ -1,25 +1,25 @@
 import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import passport from "passport";
-import cors from "cors";
-import connectDB from "./config/db.js";
-import errorHandler from "./middleware/errorHandler.js";
-import authRoutes from "./routes/authRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import walletRoutes from "./routes/walletRoutes.js";
-import classRoutes from "./routes/classRoutes.js";
-import callRoutes from "./routes/callRoutes.js";
-import recordingRoutes from "./routes/recordingRoutes.js";
-import streamTokenRouter from "./routes/streamTokenRoutes.js";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import passport from "passport";
+// import cors from "cors";
+// import connectDB from "./config/db.js";
+// import errorHandler from "./middleware/errorHandler.js";
+// import authRoutes from "./routes/authRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+// import walletRoutes from "./routes/walletRoutes.js";
+// import classRoutes from "./routes/classRoutes.js";
+// import callRoutes from "./routes/callRoutes.js";
+// import recordingRoutes from "./routes/recordingRoutes.js";
+// import streamTokenRouter from "./routes/streamTokenRoutes.js";
 
-connectDB();
+// connectDB();
 
 const app = express();
-app.use(cors({ origin: "*" }));
-app.use(express.json());
-app.use(passport.initialize());
-import("./config/passport.js").then((mod) => mod.default(passport));
+// app.use(cors({ origin: "*" }));
+// app.use(express.json());
+// app.use(passport.initialize());
+// import("./config/passport.js").then((mod) => mod.default(passport));
 
 // app.use("/api/auth", authRoutes);
 // app.use("/api/users", userRoutes);
@@ -31,7 +31,7 @@ import("./config/passport.js").then((mod) => mod.default(passport));
 app.get("/", (req, res) => {
   res.send("working !!!!");
 });
-app.use(errorHandler);
+// app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
