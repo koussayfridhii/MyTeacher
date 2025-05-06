7c05f53fa1dc141adbc29b5e2e691f63d8537602
koussayfridhii <koussayfridhi.kf@gmail.com>
2025-05-06 14:33:56 +0100

    fix: Enable CORS middleware for cross-origin requests

--- back/server.js
+++ back/server.js
@@ -14,7 +14,7 @@ import errorHandler from "./middleware/errorHandler.js";
 connectDB();
 
 const app = express();
-// app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*" }));
 // app.use(express.json());
 // app.use(passport.initialize());
 // import("./config/passport.js").then((mod) => mod.default(passport));
